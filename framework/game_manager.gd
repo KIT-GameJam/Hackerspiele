1dcc64da82ba8b2666f86952f6741fe88a229a1e
konsumlamm <konsumlamm@gmail.com>
2025-06-16 20:45:29 +0200

    Better UI layout

--- framework/game_manager.gd
+++ framework/game_manager.gd
@@ -5,17 +5,17 @@ var won_games := 0
 var lifes := 3
 @onready var timer: Timer = $Timer
 
-@onready var timer_label: Label = $CanvasLayer/TimerLabel
-@onready var score_label: Label = $CanvasLayer/ScoreLabel
-@onready var lifes_label: Label = $CanvasLayer/LifesLabel
+@onready var timer_label: Label = $CanvasLayer/HBoxContainer/TimerLabel
+@onready var score_label: Label = $CanvasLayer/HBoxContainer/ScoreLabel
+@onready var lifes_label: Label = $CanvasLayer/HBoxContainer/LifesLabel
 
 func _ready() -> void:
 	next_game()
 
 func _process(_delta: float) -> void:
 	timer_label.text = String.num(timer.time_left, 1) + " s"
-	score_label.text = str(won_games)
-	lifes_label.text = str(lifes)
+	score_label.text = "Score: " + str(won_games)
+	lifes_label.text = "Lifes: " + str(lifes)
 
 func next_game() -> void:
 	if current_game:
