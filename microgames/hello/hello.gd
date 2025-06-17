886d2313a9e196380103e71f366c7539c0979f55
nat-rix <upezu@student.kit.edu>
2025-06-17 21:04:11 +0200

    Really

--- microgames/hello/hello.gd
+++ microgames/hello/hello.gd
@@ -4,13 +4,15 @@ extends MicroGame
 @onready var label: Label = find_child("HelloLabel")
 var not_negations := randi_range(0, 5)
 var dont_negations := randi_range(0, 1)
+var really_count := randi_range(0, 1)
 var negations := not_negations + dont_negations
 var should_click := negations % 2 == 0
 
 func _ready() -> void:
 	var nots := "not ".repeat(not_negations)
 	var donts := "n't".repeat(dont_negations)
-	label.text = "Do{0} {1}click the button!".format([donts, nots])
+	var really := "Really do" if really_count else "Do"
+	label.text = "{0}{1} {2}click the button!".format([really, donts, nots])
 
 func _on_button_pressed() -> void:
 	if should_click:
