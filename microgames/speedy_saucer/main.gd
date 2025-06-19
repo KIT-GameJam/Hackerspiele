aaef43efa7e0170a986eb1911fcc8ad58743a3f8
konsumlamm <konsumlamm@gmail.com>
2025-06-19 20:45:37 +0200

    Simplify level selection in speedy saucer

--- microgames/speedy_saucer/main.gd
+++ microgames/speedy_saucer/main.gd
@@ -1,17 +1,14 @@
 extends MicroGame
 
 @onready var level_node = $Level
-const level_count := 3
-const level_paths := [
-	"res://microgames/speedy_saucer/levels/level0.tscn",
-	"res://microgames/speedy_saucer/levels/level1.tscn",
-	"res://microgames/speedy_saucer/levels/level2.tscn",
-] 
+const levels := [
+	preload("res://microgames/speedy_saucer/levels/level0.tscn"),
+	preload("res://microgames/speedy_saucer/levels/level1.tscn"),
+	preload("res://microgames/speedy_saucer/levels/level2.tscn"),
+]
 
 func _ready() -> void:
-	var random_level_idx = randi_range(0, level_count - 1)
-	var level_scene = load(level_paths[random_level_idx])
-	var level: Maze = level_scene.instantiate()
+	var level: Maze = levels.pick_random().instantiate()
 	level_node.add_child(level)
 	level.win.connect(_on_win)
 	level.loss.connect(_on_loose)
