7a520b57fd180af07e0838229bb1f514e41f9f00
Elias Schaut <eschaut@outlook.de>
2025-06-19 18:39:59 +0200

    add minigame speedy_saucer

--- microgames/microgames.gd
+++ microgames/microgames.gd
@@ -2,5 +2,6 @@ class_name MicroGames
 
 const scenes: Array[PackedScene] = [
 	preload("res://microgames/hello/hello.tscn"),
-	preload("res://microgames/skiing/main.tscn")
+	preload("res://microgames/skiing/main.tscn"),
+	preload("res://microgames/speedy_saucer/main.tscn")
 ]
