8bf26af2a092f3bee690a0f7d211344899481a27
konsumlamm <konsumlamm@gmail.com>
2025-06-19 20:40:53 +0200

    Fix warnings

--- microgames/speedy_saucer/entities/maze.gd
+++ microgames/speedy_saucer/entities/maze.gd
@@ -3,9 +3,9 @@ class_name Maze extends Node2D
 signal win
 signal loss
 
-func _on_level_player_exited(body: Node2D) -> void:
+func _on_level_player_exited(_body: Node2D) -> void:
 	loss.emit()
 
 
-func _on_win_zone_player_entered(body: Node2D) -> void:
+func _on_win_zone_player_entered(_body: Node2D) -> void:
 	win.emit()
