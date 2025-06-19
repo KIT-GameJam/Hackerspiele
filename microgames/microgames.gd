3ea3506042e458e384ed452d9c5d2aab9d7d1a73
nat-rix <upezu@student.kit.edu>
2025-06-19 22:17:32 +0200

    Add kaputtmachen microgame

--- microgames/microgames.gd
+++ microgames/microgames.gd
@@ -3,6 +3,6 @@ class_name MicroGames
 const scenes: Array[PackedScene] = [
 	preload("res://microgames/hello/hello.tscn"),
 	preload("res://microgames/skiing/main.tscn"),
-	preload("res://microgames/kaputtmachen/main.tscn"),
-	preload("res://microgames/speedy_saucer/main.tscn")
+	preload("res://microgames/speedy_saucer/main.tscn"),
+	preload("res://microgames/kaputtmachen/main.tscn")
 ]
