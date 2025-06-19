fffc18d21a6dcef3c2905a6914548ff84e590562
konsumlamm <konsumlamm@gmail.com>
2025-06-19 22:36:49 +0200

    More documentation

--- framework/micro_game.gd
+++ framework/micro_game.gd
@@ -9,6 +9,8 @@ enum Result { Loss, Win }
 ## Local storage that is persistent per run.
 var storage: Dictionary
 
+## The signal to send when the microgame is won or lost.
+## Only the first emitted signal will be considered.
 @warning_ignore("unused_signal")
 signal finished(result: Result)
 
