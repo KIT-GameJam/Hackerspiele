237bf00f8a9e25887ee4aa94f0147b175069b37e
Elias Schaut <eschaut@outlook.de>
2025-06-19 20:20:05 +0200

    add another level in speedy saucer

--- microgames/speedy_saucer/main.gd
+++ microgames/speedy_saucer/main.gd
@@ -1,10 +1,15 @@
 extends MicroGame
 
 @onready var level_node = $Level
-const level_scene := preload("res://microgames/speedy_saucer/levels/level0.tscn")
+const level_count := 2
+const level_scenes := [
+	preload("res://microgames/speedy_saucer/levels/level0.tscn"),
+	preload("res://microgames/speedy_saucer/levels/level1.tscn"),
+] 
 
 func _ready() -> void:
-	var level: Maze = level_scene.instantiate()
+	var random_level_idx = randi_range(0, level_count - 1)
+	var level: Maze = level_scenes[random_level_idx].instantiate()
 	level_node.add_child(level)
 	level.win.connect(_on_win)
 	level.loss.connect(_on_loose)
