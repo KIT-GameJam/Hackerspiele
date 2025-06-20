05b9059116ddb982353763a084a96571c78bf078
nat-rix <upezu@student.kit.edu>
2025-06-20 02:26:53 +0200

    Add bugfix micro game

--- microgames/microgames.gd
+++ microgames/microgames.gd
@@ -4,5 +4,6 @@ const scenes: Array[PackedScene] = [
 	preload("res://microgames/hello/hello.tscn"),
 	preload("res://microgames/skiing/main.tscn"),
 	preload("res://microgames/speedy_saucer/main.tscn"),
-	preload("res://microgames/alien_attack/main.tscn")
+	preload("res://microgames/alien_attack/main.tscn"),
+	preload("res://microgames/bugfix/bugfix.tscn")
 ]
