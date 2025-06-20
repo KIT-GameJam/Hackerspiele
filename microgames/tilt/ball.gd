05d40221c1e1b3e4231e6a874de0be45f67c1e5a
uxepd <uxepd@student.kit.edu>
2025-06-20 23:50:31 +0200

    feat: spawning workd

--- microgames/tilt/ball.gd
+++ microgames/tilt/ball.gd
@@ -1,5 +1,11 @@
 extends RigidBody2D
 
-func _physics_process(delta: float) -> void:
+var type: int = 0;
+
+func _ready() -> void:
+	position = Vector2(640, -10)
 	var gravitiy: Vector2 = Vector2(0, 1)
 	self.add_constant_central_force(gravitiy)
+	#var type: int = randi_range(0, 3)
+	#self.modulate = Color(1, 0, 0)
+	
