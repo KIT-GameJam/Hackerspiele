4ce67b1ab4514a01180a6b383c84c7d6721ce418
Elias Schaut <eschaut@outlook.de>
2025-06-20 02:26:05 +0200

    coin_collector: add new level

--- microgames/coin_collector/main.gd
+++ microgames/coin_collector/main.gd
@@ -3,12 +3,13 @@ extends MicroGame
 var level_scenes: Array[PackedScene] = [
 	preload("res://microgames/coin_collector/levels/level0.tscn"),
 	preload("res://microgames/coin_collector/levels/level1.tscn"),
+	preload("res://microgames/coin_collector/levels/level2.tscn"),
 ]
 var curr_level: Node2D
 var curr_level_idx: int = 0
 
 func _ready() -> void:
-	var level_idx = randi_range(0, level_scenes.size())
+	var level_idx = randi_range(0, level_scenes.size() - 1)
 	_load_level(level_idx)
 
 func _process(_delta: float) -> void:
