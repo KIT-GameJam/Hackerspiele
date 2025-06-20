320a5c17d6942bd48c06006b081b08bd6025348f
uxepd <uxepd@student.kit.edu>
2025-06-21 00:32:50 +0200

    feat: color???

--- microgames/tilt/main.gd
+++ microgames/tilt/main.gd
@@ -7,10 +7,6 @@ func _ready() -> void:
 	spawn_ball()
 	finished.emit(Result.Win)
 	
-func _process(delta: float) -> void:
-	spawn_ball()
-	
-	
 func spawn_ball():
 	var ball_obj = ball.instantiate()
 	add_child(ball_obj)
