aa2991652039124cdaaee35894acb79ecae31618
Ekaterina <77204389+e-cathy@users.noreply.github.com>
2025-06-20 18:54:59 +0200

    Add Flyswatter Microgame (#5)

--- microgames/microgames.gd
+++ microgames/microgames.gd
@@ -11,5 +11,6 @@ const scenes: Array[PackedScene] = [
 	preload("res://microgames/martian_mike/main.tscn"),
 	preload("res://microgames/nox_box/main.tscn"),
 	preload("res://microgames/skiing/main.tscn"),
-	preload("res://microgames/speedy_saucer/main.tscn"),
+	preload("res://microgames/flyswatter/fly_swatter.tscn"),
+	preload("res://microgames/speedy_saucer/main.tscn")
 ]
