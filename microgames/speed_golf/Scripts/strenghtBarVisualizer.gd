9a5459320ce50f2331d360a5878256759bda12b7
SwaggiSpaghetti <jesko.kissner@mailbox.org>
2025-06-21 21:29:02 +0200

    Finished

--- microgames/speed_golf/Scripts/strenghtBarVisualizer.gd
+++ microgames/speed_golf/Scripts/strenghtBarVisualizer.gd
@@ -1,17 +1,6 @@
 extends Sprite2D
-var pressedDown = false
-var currentStrenght = 0
-var strenghtGrow = 3
 
 func _process(delta: float) -> void:
-	if pressedDown && Input.is_action_pressed("submit"):
-		currentStrenght = min(currentStrenght + strenghtGrow *delta, 1)
-	elif Input.is_action_pressed("submit"):
-		pressedDown = true
-	else:
-		pressedDown = false
-		currentStrenght = 0
-	
-	#region_rect.size.y = 19 * (1- currentStrenght)
+	var currentStrenght = get_parent().get_parent().get_parent().strokeStrength
 	region_rect.position.y = 19 * (1-currentStrenght)
 	scale.y = (currentStrenght)
