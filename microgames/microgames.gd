8e410b9f2ad80c6c0444aac31640d04e1153b7e9
Pankarinko <pankarinko@gmail.com>
2025-06-21 17:59:06 +0200

    Quit after win and increase playtime

--- microgames/microgames.gd
+++ microgames/microgames.gd
@@ -2,23 +2,25 @@ class_name MicroGames
 
 const scenes: Array[PackedScene] = [
 	# sort alphabetically
-	preload("res://microgames/alien_attack/main.tscn"),
-	preload("res://microgames/ballon_popper/main.tscn"),
-	preload("res://microgames/bugfix/bugfix.tscn"),
+	#preload("res://microgames/alien_attack/main.tscn"),
+	#preload("res://microgames/ballon_popper/main.tscn"),
+	#preload("res://microgames/bugfix/bugfix.tscn"),dddddssaawsds
 	preload("res://microgames/calc/calc.tscn"),
-	preload("res://microgames/coin_collector/main.tscn"),
-	preload("res://microgames/crabfight/crabfight.tscn"),
-	preload("res://microgames/etch_a_sketch/main.tscn"),
-	preload("res://microgames/flyswatter/fly_swatter.tscn"),
-	preload("res://microgames/gardening/main.tscn"),
-	preload("res://microgames/hello/hello.tscn"),
-	preload("res://microgames/kaputtmachen/main.tscn"),
-	preload("res://microgames/labyrinth/main.tscn"),
-	preload("res://microgames/martian_mike/main.tscn"),
-	preload("res://microgames/nox_box/main.tscn"),
-	preload("res://microgames/responsibility/responsibility.tscn"),
-	preload("res://microgames/skiing/main.tscn"),
-	preload("res://microgames/skys/main.tscn"),
-	preload("res://microgames/speedy_saucer/main.tscn"),
-	preload("res://microgames/toast/main.tscn"),
+
+
+	#preload("res://microgames/coin_collector/main.tscn"),
+	#preload("res://microgames/crabfight/crabfight.tscn"),
+	#preload("res://microgames/etch_a_sketch/main.tscn"),
+	#preload("res://microgames/flyswatter/fly_swatter.tscn"),
+	#preload("res://microgames/gardening/main.tscn"),
+	#preload("res://microgames/hello/hello.tscn"),
+	#preload("res://microgames/kaputtmachen/main.tscn"),
+	#preload("res://microgames/labyrinth/main.tscn"),
+	#preload("res://microgames/martian_mike/main.tscn"),
+	#preload("res://microgames/nox_box/main.tscn"),
+	#preload("res://microgames/responsibility/responsibility.tscn"),
+	#preload("res://microgames/skiing/main.tscn"),
+	#preload("res://microgames/skys/main.tscn"),
+	#preload("res://microgames/speedy_saucer/main.tscn"),
+	#preload("res://microgames/toast/main.tscn"),
 ]
