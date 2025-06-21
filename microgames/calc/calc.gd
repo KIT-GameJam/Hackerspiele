be250c2d557adb90b8a60bde6ad96a69734fb680
nat-rix <upezu@student.kit.edu>
2025-06-21 03:07:17 +0200

    Fix calculator aspect ratio problems

--- microgames/calc/calc.gd
+++ microgames/calc/calc.gd
@@ -3,9 +3,11 @@ extends MicroGame
 var result = 0;
 var user_input = "";
 var correct = false;
+@onready var user_input_node = find_child("UserInput")
+@onready var task_node = find_child("Task")
 
 func _ready() -> void:
-	$CanvasLayer/Panel/UserInput.label_settings.font_color = Color("#d75145")
+	user_input_node.label_settings.font_color = Color("#d75145")
 	var first := randi_range(-10, 10);
 	var second := randi_range(-100, 100);
 	var op_index := randi_range(0, 2);
@@ -22,14 +24,14 @@ func _ready() -> void:
 			op = "+"
 			result = first + second
 	if first < 0 && second < 0:
-		$CanvasLayer/Panel/Task.text = "({0}){1}({2})".format([first, op, second])
+		task_node.text = "({0}){1}({2})".format([first, op, second])
 	elif first < 0:
-		$CanvasLayer/Panel/Task.text = "({0}){1}{2}".format([first, op, second])
+		task_node.text = "({0}){1}{2}".format([first, op, second])
 	elif second < 0:
-		$CanvasLayer/Panel/Task.text = "{0}{1}({2})".format([first, op, second])
+		task_node.text = "{0}{1}({2})".format([first, op, second])
 	else:
-		$CanvasLayer/Panel/Task.text = "{0}{1}{2}".format([first, op, second])
-	$"CanvasLayer/Panel/7".grab_focus.call_deferred()
+		task_node.text = "{0}{1}{2}".format([first, op, second])
+	find_child("7").grab_focus.call_deferred()
 	for button in find_children("*", "Button"):
 		button.pressed.connect(_on_click.bind(button))
 
@@ -42,18 +44,18 @@ func _on_click(button: Button) -> void:
 			user_input = user_input.erase(user_input.length() - 1, 1)
 		"=":
 			if int(user_input) == result:
-				$CanvasLayer/Panel/UserInput.label_settings.font_color = Color("#669e5d")
+				user_input_node.label_settings.font_color = Color("#669e5d")
 				correct = true
 			else:
-				$CanvasLayer/Panel/UserInput.label_settings.font_color = Color("#a70233")
+				user_input_node.label_settings.font_color = Color("#a70233")
 				await get_tree().create_timer(0.1).timeout
-				$CanvasLayer/Panel/UserInput.label_settings.font_color = Color("#d75145")
+				user_input_node.label_settings.font_color = Color("#d75145")
 
 				#await get_tree().create_timer(0.5).timeout
-				#$CanvasLayer/Panel/UserInput.label_settings.font_color = Color(215, 81, 69)
+				#user_input_node.label_settings.font_color = Color(215, 81, 69)
 		_:
 			user_input += button.name
-	$CanvasLayer/Panel/UserInput.text = "{0}".format([user_input])
+	user_input_node.text = "{0}".format([user_input])
 
 func on_timeout() -> Result:
 	return Result.Win if correct else Result.Loss
