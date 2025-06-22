d61da2837a213fe10f100fae63b8bbb8074e8f34
Johannes Beisswenger <johannes@beisswenger.name>
2025-06-22 04:47:11 +0200

    skys: obstacle texture, random foo

--- microgames/skys/obstacle.gd
+++ microgames/skys/obstacle.gd
@@ -6,11 +6,15 @@ func _ready() -> void:
 	#connect("body_entered", _on_body_entered)
 	#print(contact_monitor)
 	#print(max_contacts_reported)
+var height : float = 1.;
 
-func set_height(height: float):
+func set_height(height_: float):
+	height = height_
 	$Mesh.scale.y = height
 	$CollisionBox.scale.y = height
 	$Area2D/CollisionArea.scale.y = height
+
+	#$Mesh.material.set("shader_parameter/scale", height);
 	
 func _on_body_entered(body: Node) -> void:
 	# Does not work for character bodys :(
